a30ff2c583cf8b0c6e5cc4730bb65d95e894eadb
Dylan Wintermute <dylan.wintermute@cnu.edu>
2019-09-27 21:35:16 -0400

    finsished with 100

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -15,7 +15,6 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <vector>
 
 using namespace std;
 using namespace constants;
@@ -23,15 +22,13 @@ using namespace constants;
 //	stuff you will need
 //============================================================================
 //TODO define a structure to track words and number of times they occur
-const std::string MYFILE = TEST_DATA_FULL;
-
 
 struct entry {
-	string word;
+	std::string word;
 	int number_occurences;
 };
 
-entry words [MAX_WORDS];
+entry words[MAX_WORDS];
 
 int nextAvailable = 0;
 
@@ -40,110 +37,123 @@ int nextAvailable = 0;
 //TODO add variable to keep track of next available slot in array
 
 //TODO define all functions in header file
-bool openFile(std::fstream& myfile, const std::string& myFileName,
-		std::ios_base::openmode mode){
-	myfile.open(myFileName.c_str(), mode);
-	return false;
+bool openFile(std::fstream &myfile, const std::string &myFileName,
+		std::ios_base::openmode mode) {
+	myfile.open(myFileName.c_str());
+	return myfile.is_open();
 }
-void closeFile(std::fstream& myfile){
-	if (myfile.is_open()){
+void closeFile(std::fstream &myfile) {
+	if (myfile.is_open()) {
 		myfile.close();
 	}
 
 }
-int writeArraytoFile(const std::string &outputfilename){
+int writeArraytoFile(const std::string &outputfilename) {
 	ofstream myfile;
-	myfile.open(outputfilename);
-	for(int i = 0; i < nextAvailable; i++){
-		myfile << words[i].word;
+	myfile.open(outputfilename.c_str());
+	if (getArraySize() == 0) {
+		return FAIL_NO_ARRAY_DATA;
+	}
+	if (!myfile.is_open()) {
+		return FAIL_FILE_DID_NOT_OPEN;
+	}
+
+	for (int i = 0; i < getArraySize(); i++) {
+		myfile << words[i].word + " " + intToString(words[i].number_occurences)
+				<< endl;
 	}
 	myfile.close();
-	return 0;
+	return SUCCESS;
 }
-void sortArray(constants::sortOrder so){
+void sortArray(constants::sortOrder so) {
 	switch (so) {
-	case constants::ASCENDING:
-		for(int i = 0; i<nextAvailable; i++) {
-		      int swaps = 0;
-		      for(int j = 0; j<nextAvailable-i-1; j++) {
-		         if(words[j].word > words[j+1].word) {
-		        	entry temp;
-		        	temp.word = words[j].word;
-		        	words[j].word = words[j+1].word;
-		        	words[j+1].word = temp.word;
-		            swaps = 1;
-		         }
-		      }
-		      if(!swaps)
-		         break;
-		   }
-		break;
+	case constants::ASCENDING: {
+		entry temp;
+		for (int i = 0; i < nextAvailable - 1; i++) {
 
-		case constants::NONE:
-			break;
-		case constants::DESCENDING:
-			break;
-		case constants::NUMBER_OCCURRENCES:
-			break;
-	}
+			for (int j = i + 1; j < nextAvailable; j++) {
+				std::string w2 = words[j].word;
+				std::string w1 = words[i].word;
 
+				toUpper(w1);
+				toUpper(w2);
+				if (w1 > w2) {
 
-}
-void clearArray(){
-	nextAvailable = 0;
-}
+					temp = words[i];
+					words[i] = words[j];
+					words[j] = temp;
 
-int getArraySize(){
-	return nextAvailable;
-}
-void processToken(std::string &token){
-	if (strip_unwanted_chars(token)) {
-		for (int i = nextAvailable - 1; i < nextAvailable; i++){
-			if(words[i].word == token){
-				words[i].number_occurences++;
-			}
-			else {
-				entry a;
-				a.word = token;
-				a.number_occurences = 1;
-				words[nextAvailable] = a;
-				std::cout<<nextAvailable;
-				nextAvailable++;
-
+				}
 			}
 		}
+		break;
+	}
+	case constants::NONE:
+		break;
+	case constants::DESCENDING:
+		break;
+	case constants::NUMBER_OCCURRENCES:
+		break;
 	}
 
-
-	}
-
-
-void processLine(std::string &myString){
+}
+void clearArray() {
 	nextAvailable = 0;
+}
+
+int getArraySize() {
+	return nextAvailable;
+}
+void processToken(std::string &token) {
+	if (!strip_unwanted_chars(token)) {
+		return;
+	}
+
+	for (int i = 0; i < nextAvailable; i++) {
+		std::string x = words[i].word;
+		std::string t = token;
+		toUpper(t);
+		toUpper(x);
+		if (x == t) {
+			words[i].number_occurences++;
+			return;
+		}
+
+	}
+
+	if (token.length() != 0) {
+		words[nextAvailable] = entry();
+		words[nextAvailable].number_occurences = 1;
+		words[nextAvailable].word = token;
+		nextAvailable++;
+	}
+
+}
+
+void processLine(std::string &myString) {
 	stringstream ss(myString);
-	cout<<myString;
 	string tempToken;
-	while(getline(ss, tempToken, CHAR_TO_SEARCH_FOR)){
+	while (getline(ss, tempToken, CHAR_TO_SEARCH_FOR)) {
 		processToken(tempToken);
 	}
 
 }
-bool processFile(std::fstream &myfstream){
-	openFile(myfstream, MYFILE, ios::in);
+bool processFile(std::fstream &myfstream) {
 	std::string line;
-	if (!myfstream.is_open()){
+	if (!myfstream.is_open()) {
 		return false;
 	}
 	while (!myfstream.eof()) {
+		getline(myfstream, line);
 		processLine(line);
 
-		}
+	}
 	return true;
 }
-int getArrayWord_NumbOccur_At(int i){
+int getArrayWord_NumbOccur_At(int i) {
 	return words[i].number_occurences;
 }
-std::string getArrayWordAt(int i){
+std::string getArrayWordAt(int i) {
 	return words[i].word;
 }
 
